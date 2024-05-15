6a8a58a4d168f6deea425e24ea564e01aa7cb7da
manuu1999 <manuel.buser@students.fhnw.ch>
2024-05-15 10:47:46 +0200

    Initial commit

---
+++ src/main/java/com/example/TechTitans/model/Depot.java
@@ -0,0 +1,29 @@
+package com.example.TechTitans.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "depot")
+public class Depot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "depot_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "depot_address", nullable = false, length = 100)
+    private String depotAddress;
+
+    @Column(name = "size_capacity_in_m3", nullable = false)
+    private Float sizeCapacityInM3;
+
+    @Column(name = "weight_capacity_in_kg", nullable = false)
+    private Float weightCapacityInKg;
+
+    // Getters and setters
+}
